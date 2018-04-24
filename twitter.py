f1b53e4f147d7c3520c4f04aadc7b7058683977b
Nilay Pachauri <pachauri@usc.edu>
2018-04-24 04:39:04 -0700

    Found the current location

--- twitter.py
+++ twitter.py
@@ -1,4 +1,12 @@
+import json
+import requests
 import tweepy
+
+# Extracts a json packet from the webiste and returns a json packet of strings
+def get_cur_loc():
+	send_url = 'https://ipinfo.io'
+	r = requests.get(send_url)
+	return json.loads(r.text, "utf-8")
 
 # Main function
 def main():
@@ -12,7 +20,8 @@ def main():
 
 	# Get functions
 	api = get_api(cfg)
-	
+	cur_loc = get_cur_loc()
+
 	tweet = "Hello World"
 	status = api.update_status(status=tweet)
 
