693312c06aab9fd6c9899615bc97ec7e131f32b1
Nilay Pachauri <pachauri@usc.edu>
2018-04-24 04:52:08 -0700

    Getting what inside actually feels like

--- twitter.py
+++ twitter.py
@@ -93,6 +93,7 @@ def main():
 	# Store the inside value data
 	in_temp = float(sys.argv[1])
 	in_humidity = int(sys.argv[2])
+	in_heat_index = get_heat_index(in_temp, in_humidity)
 
 	tweet = temp_msg + " in " + org + ", " + city + ", " + region + " " + postal
 	status = api.update_status(status=tweet)
