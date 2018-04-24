3ca4e796debe99d1ceab0855113089f23121cf1d
Nilay Pachauri <pachauri@usc.edu>
2018-04-24 04:47:12 -0700

    Grabbed individual temperature and humidity data

--- twitter.py
+++ twitter.py
@@ -37,7 +37,9 @@ def main():
 	api = get_api(cfg)
 	cur_loc = get_cur_loc()
 	temp_and_humidity = get_temp_and_humidity(float(cur_loc['loc'].split(',')[0]), float(cur_loc['loc'].split(',')[1]))
-
+	out_temp = temp_and_humidity[0]['temp']
+	out_humidity = temp_and_humidity[1]
+	
 	temp_msg = ""
 
 	org = " ".join(cur_loc['org'].split()[1:])
