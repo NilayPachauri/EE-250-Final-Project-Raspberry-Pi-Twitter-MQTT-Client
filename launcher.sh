f3819d2d62d0e94520e0012ff156b9c96cf25f49
Nilay Pachauri <pachauri@usc.edu>
2018-04-28 00:23:11 -0700

    Added a launcher script which executes on Raspberry Pi startup

---
+++ launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/pi/Nilay/EE-250-Final-Project-Raspberry-Pi-Twitter-MQTT-Client/
+python3 client.py
+cd /
